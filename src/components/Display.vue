7e6e794b53b47c5a2161123a0dfdfdaea08ab91c
Muffadal Khopoliwala <muffadal.khopoliwala@v2solutions.com>
2018-02-22 13:14:09 +0530

    Added VueJsAssignment

---
+++ src/components/Display.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div v-for="(item, index) in NameList">
+    FirstName: {{item.FirstName}}    
+    MiddleName: {{item.MiddleName}}
+    LastName: {{item.LastName}}
+    FullName: {{item.FullName}}
+    <button @click="EditUser(index)">Edituser</button>
+  </div>  
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Display',
+    
+    methods: {
+      EditUser : function(Index) {        
+        debugger
+          this.$emit('event_edit_view', Index)
+      }
+    },
+    props: {
+    NameList: {
+      default: []
+    }         
+  } 
+}
+</script>
+
+
+
+
---
+++ src/components/User.vue
@@ -0,0 +1,40 @@
+<template>
+<div>  
+   <div>
+   FirstName <input v-model="FirstName" placeholder="FirstName"/>
+   MiddleName <input v-model="MiddleName" placeholder="MiddleName"/>
+   LastName <input v-model="LastName" placeholder="LastName"/> 
+   <button v-on:click="AddUser">AddUser</button>      
+   </div>      
+   <display :name-list="NameList" v-on:event_edit_view="editUser"></display>    
+</div> 
+</template>
+
+<script>
+import Display from './Display.vue'
+     export default {
+        name: 'User',
+        components: {
+            Display
+        },
+        data(){
+            return {
+               FirstName: '',
+               MiddleName:'',
+               LastName:'',
+               NameList: []
+            }
+        },                 
+        methods: {
+        AddUser: function () {
+        this.NameList.push({'FirstName' : this.FirstName,'MiddleName': this.MiddleName, 'LastName':this.LastName, 'FullName': this.FirstName + this.MiddleName + this.LastName})        
+         },
+         editUser: function(index)
+         {     debugger   
+             this.FirstName =  this.NameList[index].FirstName,
+             this.MiddleName = this.NameList[index].MiddleName,
+             this.LastName = this.NameList[index].LastName             
+         }
+      }      
+    }
+</script>
